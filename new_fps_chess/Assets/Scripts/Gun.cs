415b3c41b6ea53caa659f9d380016ac24a02c3c5
Rafael Loo <loorafael@hotmail.com>
2023-12-15 22:36:16 -0800

    fixed shooting

--- new_fps_chess/Assets/Scripts/Gun.cs
+++ new_fps_chess/Assets/Scripts/Gun.cs
@@ -31,8 +31,15 @@ public class Gun : MonoBehaviour
     void Shoot()
     {
         Debug.Log("shoot!");
-        GameObject bullet = Instantiate(bulletPrefab, bulletPoint.transform.position, transform.rotation);
-        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+        GameObject bullet = Instantiate(bulletPrefab, bulletPoint.transform.position, Quaternion.identity); // Use no rotation upon instantiation
+
+        // Calculate the correct world space forward direction
+        Vector3 worldSpaceForward = bulletPoint.transform.TransformDirection(Vector3.forward);
+
+        bullet.GetComponent<Rigidbody>().AddForce(worldSpaceForward * bulletSpeed);
         Destroy(bullet, 2);
+
+        // Debug line to visualize the forward direction in world space
+        Debug.DrawLine(bullet.transform.position, bullet.transform.position + worldSpaceForward * 10, Color.red, 2);
     }
 }
